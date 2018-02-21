e67a1b3cbb97fff3c0553bd9443bdda8efba0a15
EugeneLAT <jevgenijs.kobiluks@inbox.lv>
2018-02-21 06:43:39 +0200

    Version 1.1

--- src/TsiJava/Main.java
+++ src/TsiJava/Main.java
@@ -9,28 +9,40 @@ public class Main {
 
         Random random = new Random();
         Scanner scanner = new Scanner(System.in); // System.in считывает что вел пользователь
-
         int RNum = random.nextInt(100) + 1;
-        System.out.println("I think of number from 1 to 100. Try to guess it.");
-        //System.out.println(RNum);
+        String Answer;
+
+        do {
+            int Attempts = 10;
+            System.out.println("I think of number from 1 to 100. You have " + Attempts + " attempts!");
+            System.out.println("Good luck!");
+            System.out.println(RNum);
 
 
-        for (int i = 0; true ; i++) {
+            for (int i = 0; i < Attempts; i++) {
 
-            System.out.println("Its your " + (i + 1) + " try!");
-            System.out.print("Please add your number:");
+                System.out.println("Its your " + (i + 1) + " try!");
+                System.out.print("Please add your number: ");
 
-            int UserNum = scanner.nextInt();
+                int UserNum = scanner.nextInt();
 
-            if (UserNum > RNum) {
-                System.out.println("Your number " + UserNum + " are bigger than my!");
-            } else if (UserNum < RNum) {
-                System.out.println("Your number " + UserNum + " are smaller than my!");
-            } else {
-                System.out.println("Congratulate!");
-                break;
+                if (UserNum > RNum) {
+                    System.out.println("Your number " + UserNum + " are bigger than my!");
+                } else if (UserNum < RNum) {
+                    System.out.println("Your number " + UserNum + " are smaller than my!");
+                } else {
+                    System.out.println("Congratulate!");
+                    break;
+                }
+
             }
+            System.out.println("Sorry! You lost all " + Attempts + " attempts!");
 
-        }
+            System.out.println("Do you want to play again? ( Y / N )");
+            System.out.print("Answer: ");
+            Answer = scanner.next();
+
+
+        }while(Answer.equals("Y"));
     }
 }
